3755fc1837a9b1e9f394f2c8cbe65677f29b1167
Jerie See Villanueva <codestackjs007@gmail.com>
2024-05-29 19:31:54 -0700

    Expense Form Lecture

--- expenseTracker/src/App.tsx
+++ expenseTracker/src/App.tsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+// import { categories } from "../../App";
 
+
+
+//comments
 const App = () => {
+
+  //create a useState to help us handle our selectedCategories
+const [selectedCategory, setSelectedCategory] = useState('');
+
+
   const [dummyExpensesArray, setDummyExpensesArray] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 15, category: "Entertainment" },
@@ -12,27 +22,34 @@ const App = () => {
   ]);
 
   const handleDelete = (id: number) => {
-    setDummyExpensesArray(
-      dummyExpensesArray.filter((expense) => expense.id !== id)
-    );
-  };
+    setDummyExpensesArray(dummyExpensesArray.filter((expense) => expense.id !== id));
+  }
+
+  //create a variable with a ternary operator we are going to use our selectedCategory as a boolean filter through our dummyExpensesArray
+  const visibleExpense = selectedCategory ? dummyExpensesArray.filter(e => e.category === selectedCategory) : dummyExpensesArray;
 
   return (
     <>
       <h1 className="text center">Expense Tracker </h1>
 
       <div className="m-5">
+        <ExpenseForm/>
+      </div>
+
+      <div className="m-5">
         {/* <ExpenseList expenses={dummyExpensesArray} onDelete={(id) => console.log('Delete', id)}/> */}
         {/* <ExpenseList expenses={dummyExpensesArray} onDelete={(id) => setDummyExpensesArray()}/> */}
+        <ExpenseFilter onSelectCategory={category => setSelectedCategory(category)}/>
 
         <div className="m-5">
-          <ExpenseFilter
-            onSelectCategory={(category) => console.log(category)}
-          />
+          {/* <ExpenseFilter onSelectCategory={(category) => console.log(category)}/> */}
+          {/* <ExpenseList expenses={visibleExpense} onDelete={handleDelete}/> */}
+
         </div>
       </div>
 
-      <ExpenseList expenses={dummyExpensesArray} onDelete={handleDelete} />
+      {/* <ExpenseList expenses={dummyExpensesArray} onDelete={handleDelete} /> */}
+      <ExpenseList expenses={visibleExpense} onDelete={handleDelete} />
     </>
   );
 };
---
+++ expenseTracker/src/expense-tracker/categories.ts
@@ -0,0 +1,10 @@
+
+
+
+const categories = ['Groceries', 'Utilities', 'Entertainment','Food','Shopping']
+
+
+
+
+
+export default categories
